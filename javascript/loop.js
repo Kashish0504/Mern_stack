51c91427194f43311839273f5ef35b85cfabcd0b
Kashish Gupta <kashishgupta0504@gmail.com>
2026-02-12 13:11:50 +0530

    code

--- javascript/loop.js
+++ javascript/loop.js
@@ -23,21 +23,20 @@ This is index: ${i}\n`);
 
 
 //1. print numbers from 1 to 10 
-// for (let i = 1; i <=10; i++) {
-//     console.log(i);
-// }
+for (let i = 1; i <=10; i++) {
+     console.log(i);
+}
 
 //2./3. print  all even / odd numbers  between 1 to 50  
 
-// for (let i=1; i<=50;i++)
-//  {
-//     if(i%2===0){
-//     console.log(`this is even number :- ${1}`)
-// }
-//  else {
-//     console.log(`this is odd number :-${i}`)
-//  }
-// }
+ for (let i=1; i<=50;i++)
+  {
+    if(i%2===0){
+   console.log(`this is even number :- ${1}`)
+}  else {
+     console.log(`this is odd number :-${i}`)
+  }
+ }
 
 
 // 4. print the multiplication table of 5 
@@ -75,3 +74,81 @@ let sum = 0
     console.log(`3x ${i}=${3 * i}`)
  }
  // 10. print factorial of given number
+ let num = +Process.argv[2];;
+ let fact=1
+ for (i = 1; i<= num; i++) {
+    fact=fact*i;
+
+
+ }
+ console.log(fact)
+
+
+
+ //while  loop
+
+ // 1 to 100
+
+
+ let start = 1;
+
+ let end = 100;
+
+ while (start <=end) {
+   //code
+
+
+   console.log{start};
+
+   //increments
+   start++;
+ }
+
+ let given = 5;
+
+ let start_1 = 1;
+
+ let result = 1;
+
+
+ //120
+
+ while (start_1 <= given) {
+   result *=start_1;
+   start_1++;
+
+ }
+ console.log('~result:',result);
+
+ // do while loop
+
+ let start_2 = 1;
+ do {
+   console.log{'hello'};
+   start_2++;
+
+ }while (start_2 <= 3);
+
+
+ // for in and for of 
+ let objects ={
+   animal:'cat',
+   married:false,
+   age:44,
+   city:{"pune","mumbai","lucknow","banglore"}
+   hobbie: {
+      virtual:'pub-g',
+      outDoor:'football',
+      inDoor:'carrom',
+   },
+   myName:function(){
+      console.log{this.animal};
+
+   },
+ };
+
+ for (let key in object){
+   console.log('this is key ${key}this is value',object[key]);
+ }
+
+//console.log(array[4].name);
