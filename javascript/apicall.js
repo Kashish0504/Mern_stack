da3ca9cd7b3a2261c893052263d360c69bd59f71
Kashish Gupta <kashishgupta0504@gmail.com>
2026-02-24 13:02:45 +0530

    apicall

---
+++ javascript/apicall.js
@@ -0,0 +1,19 @@
+let base_Api = ``;
+
+const Apicall = async () => {
+
+    /*  fetch(base_Api)
+    .then((res) => res.jaan()) // readablestream -> parse -> objects
+    .then ((res) => console.log(res))
+    .catch((err) => console.log(err)); */
+
+    try {
+        let res = await fetch(base_Api);
+        let value = await res . jaan();
+        console.log('~value:', value);
+
+    }catch(error){
+        console.log('~error:', error);
+    }
+};
+Apicall();
