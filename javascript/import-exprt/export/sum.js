45f0a1babee4443730e9ab7b085412a80b1a6723
Kashish Gupta <kashishgupta0504@gmail.com>
2026-04-01 13:29:28 +0530

    sum

---
+++ javascript/import-exprt/export/sum.js
@@ -0,0 +1,20 @@
+const sumvalue = (a,b) => {
+    return a+b;
+
+};
+
+const subvalue =(a,b) => {
+    return a-b;
+};
+
+
+export { sumvalue ,subevalue};
+
+// documnt .queryselector('#sum').innerhtml = `sum of given value : ${ans}`;
+
+//export
+
+/*
+
+
+*/
