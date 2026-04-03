b17f3a1e130470e2e7c7c56a9e601c73d219d32d
Kashish Gupta <kashishgupta0504@gmail.com>
2026-04-03 19:18:13 +0530

    navbar

--- javascript/import-exprt/export/navbar.js
+++ javascript/import-exprt/export/navbar.js
@@ -1,10 +1,10 @@
-export const navbar = () => {
-    return `<nav
-    style="display:flex; justify-content: space-evenly; align-items: center"
-    >
-    <div>
-    img
-    width="50"
-    style="border-radius: 50%"
-    src="../../`
-}
+// export const navbar = () => {
+//     return `<nav
+//     style="display:flex; justify-content: space-evenly; align-items: center"
+//     >
+//     <div>
+//     img
+//     width="50"
+//     style="border-radius: 50%"
+//     src="../../`
+// }
